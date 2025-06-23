589773e78894cb471e65fcbad9add52f4f615266
prit chhatrala <pritchhatrala3333@gmail.com>
2025-06-23 15:10:29 +0530

    Use env variable for secret key

--- Ehousing/settings.py
+++ Ehousing/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sh09oe*x@39yq#)8ggg%=25wi%m$afgfkg@r^ig%bl#z%vq$6='
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
