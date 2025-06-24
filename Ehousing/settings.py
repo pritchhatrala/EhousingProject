8b4eb7f987c72f0cd2e19dfcee8d62b349a22530
prit chhatrala <pritchhatrala3333@gmail.com>
2025-06-24 12:58:21 +0530

    modyfied

--- Ehousing/settings.py
+++ Ehousing/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = True
 
 
-ALLOWED_HOSTS = ["https://ehousingproject-1.onrender.com/", 'localhost']
+ALLOWED_HOSTS = [".onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
